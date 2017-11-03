9a1bfd8968641cbf1da02d097a37af5d06195c5b
apara <aparant.mane@utah.edu>
2017-11-02 21:39:32 -0600

    Added saved load panel in main menu...

--- GameplayModules/Assets/Scripts/Controller/SceneManager.cs
+++ GameplayModules/Assets/Scripts/Controller/SceneManager.cs
@@ -8,20 +8,37 @@ public class SceneManager : MonoBehaviour {
     public Button button_LoadGame;
     public Button button_Quit;
     public Button button_Options;
-    public GameObject fadedPanel;
+	public Button button_LoadSavedGamesPanel;
+	public Button button_LoadSavedGame1;
+	public Button button_LoadSavedGame2;
+	public Button button_LoadSavedGame3;
+	public Button button_BackToMainMenu;
+
+	public GameObject fadedPanel;
+	public GameObject panel_LoadGames;
 
     void Start () {
 
         button_LoadGame = GameObject.Find("Canvas/Panel/Panel/Button-New GAme").GetComponent<Button>();
         button_Quit = GameObject.Find("Canvas/Panel/Panel/Button-Quit").GetComponent<Button>();
         button_Options = GameObject.Find("Canvas/Panel/Panel/Button-Options").GetComponent<Button>();
+		button_LoadSavedGame1 = GameObject.Find ("Canvas/Panel/Panel_Load/Button-New GAme (1)").GetComponent<Button> ();
+		button_LoadSavedGame2 = GameObject.Find ("Canvas/Panel/Panel_Load/Button-New GAme (2)").GetComponent<Button> ();
+		button_LoadSavedGame3 = GameObject.Find ("Canvas/Panel/Panel_Load/Button-New GAme (3)").GetComponent<Button> ();
+		button_LoadSavedGamesPanel = GameObject.Find ("Canvas/Panel/Panel/Button-Load Game").GetComponent<Button> ();
+		button_BackToMainMenu = GameObject.Find ("Canvas/Panel/Panel_Load//Canvas/Panel/Panel_Load/Button-BackToMenu").GetComponent<Button> ();
+
+		fadedPanel = GameObject.Find("Canvas/Panel-ScreenFader");
+		panel_LoadGames = GameObject.Find ("Canvas/Panel/Panel_Load");
 
         button_LoadGame.onClick.AddListener(() => LoadGame());
         button_Quit.onClick.AddListener(() => Quit());
         button_Options.onClick.AddListener(() => LoadOptions());
+		button_BackToMainMenu.onClick.AddListener (() => BackToMainMenu ());
+		button_LoadSavedGamesPanel.onClick.AddListener (() => OpenLoadGamesPlanel ());
 
-        fadedPanel = GameObject.Find("Canvas/Panel-ScreenFader");
         fadedPanel.SetActive(false);
+		panel_LoadGames.SetActive (false);
     }
 	
     public void Quit() {
@@ -54,4 +71,12 @@ public class SceneManager : MonoBehaviour {
     public void InvokedChaged_Quit() {
         Application.Quit(); 
     }
+
+	public void OpenLoadGamesPlanel(){
+		panel_LoadGames.SetActive (true);
+	}
+
+	public void BackToMainMenu(){	
+		panel_LoadGames.SetActive (false);
+	}
 }
