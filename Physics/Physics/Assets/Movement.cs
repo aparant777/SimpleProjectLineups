a8b364df772d874d678bfe71a61e2cdabda12535
apara <aparant.mane@utah.edu>
2017-11-22 00:03:21 -0700

    Final Commit-ASsignment8

---
+++ Physics/Physics/Assets/Movement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+    float rotateSpeed = 10;
+    public float speed = 5;
+    private int timesHit = 0;
+
+    public GameObject gear1;
+    public GameObject gear2;
+    public GameObject cylinder;
+
+    //float speed;
+    float width = 4;
+    float height = 7;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButton("Jump"))
+        {
+            gear1.transform.Rotate(0, 40 * Time.deltaTime, 0);
+        //    gear1.transform.rotation = Quaternion.Lerp(this.transform.rotation, Quaternion.Euler(0, timesHit * 90, 0), Time.deltaTime * speed);
+        gear2.transform.Rotate(0, -40 * Time.deltaTime, 0);
+
+            //cylinder.transform.Translate(0, 0, Time.deltaTime * 0.1f); // move forward
+           // cylinder.transform.Rotate(0, Time.deltaTime * 0.1f, 0); // turn a little
+        }
+
+        // this.transform.rotation = Quaternion.Lerp(this.transform.rotation, Quaternion.Euler(0, timesHit * 90, 0), Time.deltaTime * speed);
+    }
+}
---
+++ Physics/Physics/Assets/TriggerPad.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerPad : MonoBehaviour {
+
+	public GameObject cage;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void OnTriggerEnter(Collider other){
+	
+		if (other.gameObject.tag == "Ball") {
+			Rigidbody rigidbody = cage.GetComponent<Rigidbody> ();
+			rigidbody.useGravity = true;
+		}
+
+	}
+}
